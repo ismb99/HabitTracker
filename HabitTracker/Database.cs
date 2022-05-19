91ffb9d02cd9d488891e186b4cb54acfc0c87772
ismb99 <54552069+ismb99@users.noreply.github.com>
2022-05-19 12:47:38 +0200

    Added classes

---
+++ HabitTracker/Database.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitTracker
+{
+    public class Database
+    {
+       private const string connectionString = @"URI=file:C:\Users\Ismail\Desktop\habit1.db";
+
+        public void DatabaseProvider()
+        {
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = @"Create table if not EXISTS  drinking_cola(id integer primary key autoincrement,Date Text, Quantity integer)";
+                command.ExecuteNonQuery();
+                connection.Close();
+            };
+        }
+
+
+        // Show all records in the database
+        public static void DisplayAll()
+        {
+            Console.Clear();
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = @"Select * From drinking_cola";
+
+                SQLiteCommand cmd = new SQLiteCommand(command.CommandText, connection);
+                SQLiteDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader.GetInt32(0)} {reader.GetString(1)} {reader.GetInt32(2)}\n");
+                }
+                connection.Close();
+            };
+        }
+
+        public static void Instert()
+        {
+            Console.Clear();
+            string date = ProgramHelpers.GetDate();
+            var quantity = ProgramHelpers.GetUserInputQuantity();
+
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = $"INSERT INTO drinking_cola(Date, Quantity) VALUES('{date}', {quantity})";
+                command.ExecuteNonQuery();
+                connection.Close();
+            };
+        }
+
+
+
+        public static void Delete()
+        {
+            Console.Clear();
+            DisplayAll();
+
+            Console.WriteLine("Choose the id you want to remove");
+            string idInput = Console.ReadLine();
+
+            if (idInput == "0") ProgramHelpers.MainMenu();
+
+            int numInput = int.Parse(idInput);
+
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = $"DELETE FROM drinking_cola WHERE id= {numInput}";
+
+                command.ExecuteNonQuery();
+                connection.Close();
+            };
+        }
+
+        public static void Update()
+        {
+            DisplayAll();
+
+            Console.WriteLine("Choose the id you want to update");
+
+            string id = Console.ReadLine();
+            int numId = int.Parse(id);
+
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                var checkCmd = connection.CreateCommand();
+                checkCmd.CommandText = $"SELECT EXISTS(SELECT 1 FROM drinking_cola WHERE Id = {numId})";
+                int checkQuery = Convert.ToInt32(checkCmd.ExecuteScalar());
+
+                if (checkQuery == 0)
+                {
+                    Console.WriteLine($"Record with id {numId} dosent exist");
+                    connection.Close();
+                    Update(); // ??
+                }
+
+                string date = ProgramHelpers.GetDate();
+                var quantity = ProgramHelpers.GetUserInputQuantity();
+
+                var command = connection.CreateCommand();
+                command.CommandText = $"UPDATE drinking_cola SET date = '{date}', quantity = {quantity} WHERE Id = {numId}";
+                command.ExecuteNonQuery();
+                connection.Close();
+
+            };
+        }
+
+    }
+
+
+
+}
---
+++ HabitTracker/ProgramHelpers.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Globalization;
+
+namespace HabitTracker
+{
+    public static class ProgramHelpers
+    {
+
+        public static void MainMenu()
+        {
+            bool closeApp = false;
+            while (!closeApp)
+            {
+                Console.WriteLine("Welcome to the habit tracker, make a choice\n\n");
+                Console.WriteLine("0 to quit");
+                Console.WriteLine("1 show all records");
+                Console.WriteLine("2 insert");
+                Console.WriteLine("3 Delete");
+                Console.WriteLine("4 Update");
+                Console.WriteLine("---------------------------\n\n");
+
+                string Userinput = Console.ReadLine();
+
+                switch (Userinput)
+                {
+                    case "0":
+                        Console.WriteLine("Goodbye");
+                        closeApp = true;
+                        break;
+
+                    case "1":
+                        Database.DisplayAll();
+                        break;
+
+                    case "2":
+                        Database.Instert();
+                        break;
+
+                    case "3":
+                        Database.Delete();
+                        break;
+
+                    case "4":
+                        Database.Update();
+                        break;
+
+                    default:
+                        Console.WriteLine("wrong choice, you can only choose between 0-4\n");
+                        break;
+                }
+            }
+        }
+
+
+        public static int GetUserInputQuantity()
+        {
+            Console.Write("Type the Quantity: ");
+            string qytStr = Console.ReadLine();
+
+            if (qytStr == "0") ProgramHelpers.MainMenu();
+
+            int quantity = int.Parse(qytStr);
+
+            bool isValid = false;
+
+            while (!isValid)
+            {
+                if(quantity < 0)
+                {
+                    while (quantity < 0)
+                    {
+                        Console.WriteLine("Quantity must be more then 0, try again");
+                        quantity = int.Parse(Console.ReadLine());
+                    }
+                }
+               
+                Console.WriteLine($"Qutantity {quantity} added to db\n");
+                isValid = true;
+           
+            }
+            return quantity;
+        }
+
+        public static string GetDate()
+        {
+            Console.WriteLine("\n\nPlease insert the date: (Format: yyyy-mm-dd). Type 0 to return to main manu.\n\n");
+            string date = Console.ReadLine();
+            if (date == "0") ProgramHelpers.MainMenu();
+
+            while (!DateTime.TryParseExact(date, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out _))
+            {
+                Console.WriteLine("\n\nInvalid date. (Format: yyyy-mm-dd hh:mm). Type 0 to return to main manu or try again:\n\n");
+                date = Console.ReadLine();
+            }
+            return date;
+        }
+    }
+}
